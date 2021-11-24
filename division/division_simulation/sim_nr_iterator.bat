28b328d3cc8b0d43304ee918cc1cce6cb18bc313
jari <jaarhonka@yahoo.com>
2021-11-24 07:41:10 +0200

    remove vimfiles

--- division/division_simulation/sim_nr_iterator.bat
+++ division/division_simulation/sim_nr_iterator.bat
@@ -4,16 +4,15 @@ rem simulate tb_nr_iterator.vhd
 FOR /F "tokens=* USEBACKQ" %%F IN (`git rev-parse --show-toplevel`) DO (
 SET project_root=%%F
 )
-SET source=%project_root%/source
+SET source=%project_root%/
 
-
-ghdl -a --ieee=synopsys --work=math_library %source%/math_library/multiplier/multiplier_pkg.vhd
-ghdl -a --ieee=synopsys --work=math_library %source%/math_library/division/division_internal_pkg.vhd
-ghdl -a --ieee=synopsys --work=math_library %source%/math_library/division/division_pkg.vhd
-ghdl -a --ieee=synopsys --work=math_library %source%/math_library/division/division_pkg_body.vhd
+ghdl -a --ieee=synopsys --work=math_library %source%/multiplier/multiplier_pkg.vhd
+ghdl -a --ieee=synopsys --work=math_library %source%/division/division_internal_pkg.vhd
+ghdl -a --ieee=synopsys --work=math_library %source%/division/division_pkg.vhd
+ghdl -a --ieee=synopsys --work=math_library %source%/division/division_pkg_body.vhd
 ghdl -a --ieee=synopsys tb_nr_iterator.vhd
 ghdl -e --ieee=synopsys tb_nr_iterator
-ghdl -r --ieee=synopsys tb_nr_iterator --vcd=tb_nr_iterator.vcd
+ghdl -r --ieee=synopsys tb_nr_iterator --wave=tb_nr_iterator.ghw
 
 
 IF %1 EQU 1 start "" gtkwave tb_nr_iterator.vcd
