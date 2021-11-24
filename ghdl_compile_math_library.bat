6109190c38ef334ba0b58a1502bc28761555c0a5
jari <jaarhonka@yahoo.com>
2021-11-24 08:14:28 +0200

    add ghdl_compile_math_library.bat for compiling math library for syntax checking

---
+++ ghdl_compile_math_library.bat
@@ -0,0 +1,15 @@
+echo off
+
+echo %project_root%
+FOR /F "tokens=* USEBACKQ" %%F IN (`git rev-parse --show-toplevel`) DO (
+SET project_root=%%F
+)
+SET source=%project_root%/../
+
+ghdl -a --ieee=synopsys --work=math_library %source%/math_library/multiplier/multiplier_pkg.vhd
+ghdl -a --ieee=synopsys --work=math_library %source%/math_library/sincos/sincos_pkg.vhd
+ghdl -a --ieee=synopsys --work=math_library %source%/math_library/division/division_internal_pkg.vhd
+ghdl -a --ieee=synopsys --work=math_library %source%/math_library/division/division_pkg.vhd
+ghdl -a --ieee=synopsys --work=math_library %source%/math_library/first_order_filter/first_order_filter_pkg.vhd
+ghdl -a --ieee=synopsys --work=math_library %source%/math_library/pi_controller/pi_controller_pkg.vhd
+
