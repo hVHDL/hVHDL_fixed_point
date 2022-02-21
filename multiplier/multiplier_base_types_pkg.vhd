5e92c234f26a3172cd22034ade4ba66eb3dea884
jari <jaarhonka@yahoo.com>
2022-02-22 01:01:14 +0200

    add constant for number of input bits

--- multiplier/multiplier_base_types_pkg.vhd
+++ multiplier/multiplier_base_types_pkg.vhd
@@ -4,10 +4,12 @@ library ieee;
 
 package multiplier_base_types_pkg is
 
+    constant number_of_input_bits : integer := 18;
 
-    type input_array is array (integer range 0 to 0) of signed(17 downto 0);
+
+    type input_array is array (integer range 0 to 0) of signed(number_of_input_bits-1 downto 0);
     constant init_input_array : input_array := (0=> (others => '0'));
-    type output_array is array (integer range 0 to 0) of signed(35 downto 0);
+    type output_array is array (integer range 0 to 0) of signed(init_input_array(0)'length*2-1 downto 0);
     constant init_output_array : output_array := (0=> (others => '0'));
 
     type multiplier_base_record is record
@@ -20,7 +22,7 @@ package multiplier_base_types_pkg is
     end record;
 
     constant initialize_multiplier_base : multiplier_base_record := (init_input_array, init_input_array, init_output_array, (others => '0'), false, '0');
-    constant output_word_bit_width : natural := 18;
+    constant output_word_bit_width : natural := init_input_array(0)'length;
     constant output_left_index : natural := output_word_bit_width-1;
 
 end package multiplier_base_types_pkg;
