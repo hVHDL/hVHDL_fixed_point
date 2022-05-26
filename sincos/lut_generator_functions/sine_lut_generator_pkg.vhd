57f08d5dc3c9c646734917de8384aa689186281a
jari <jaarhonka@yahoo.com>
2022-05-26 12:49:22 +0300

    make constraints for output to guarantee working lut implementation

--- sincos/lut_generator_functions/sine_lut_generator_pkg.vhd
+++ sincos/lut_generator_functions/sine_lut_generator_pkg.vhd
@@ -5,15 +5,16 @@ library ieee;
 
 
 package lookup_table_generator_pkg is
+    subtype lookup_table_generator_output is real range -1.0 to 1.0;
 ------------------------------------------------------------------------
     function sine_lookup_table_generator (
         input : real range 0.0 to 1.0)
-    return real;
+    return lookup_table_generator_output;
 ------------------------------------------------------------------------
 ------------------------------------------------------------------------
     -- this is the interface function to lookup table module
     function lookup_table_generator ( input : real range 0.0 to 1.0)
-        return real;
+        return lookup_table_generator_output;
 ------------------------------------------------------------------------
 
 end package lookup_table_generator_pkg;
@@ -24,7 +25,7 @@ package body lookup_table_generator_pkg is
     (
         input : real range 0.0 to 1.0
     )
-    return real
+    return lookup_table_generator_output
     is
     begin
         return sin(2.0*math_pi*input);
@@ -35,7 +36,7 @@ package body lookup_table_generator_pkg is
     (
         input : real range 0.0 to 1.0
     )
-    return real
+    return lookup_table_generator_output
     is
 
     begin
