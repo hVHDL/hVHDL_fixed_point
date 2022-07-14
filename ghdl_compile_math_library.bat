2ae8f2548dbd0f6bab3d7e37a08d7bd6261c1516
jari <jaarhonka@yahoo.com>
2022-07-14 09:02:10 +0300

    fix paths in ghdl_compile

--- ghdl_compile_math_library.bat
+++ ghdl_compile_math_library.bat
@@ -1,28 +1,22 @@
 echo off
 
-echo %project_root%
-FOR /F "tokens=* USEBACKQ" %%F IN (`git rev-parse --show-toplevel`) DO (
-SET project_root=%%F
-)
-SET source=%project_root%/../
+ghdl -a --ieee=synopsys --std=08 hVHDL_memory_library/fpga_ram/ram_configuration/ram_configuration_16x1024_pkg.vhd
+ghdl -a --ieee=synopsys --std=08 hVHDL_memory_library/fpga_ram/ram_read_port_pkg.vhd
 
-ghdl -a --ieee=synopsys --std=08 %source%/math_library/hVHDL_memory_library/fpga_ram/ram_configuration/ram_configuration_16x1024_pkg.vhd
-ghdl -a --ieee=synopsys --std=08 %source%/math_library/hVHDL_memory_library/fpga_ram/ram_read_port_pkg.vhd
+ghdl -a --ieee=synopsys --std=08 multiplier/multiplier_base_types_18bit_pkg.vhd
+ghdl -a --ieee=synopsys --std=08 multiplier/multiplier_pkg.vhd
+ghdl -a --ieee=synopsys --std=08 sincos/sincos_pkg.vhd
 
-ghdl -a --ieee=synopsys --std=08 %source%/math_library/multiplier/multiplier_base_types_18bit_pkg.vhd
-ghdl -a --ieee=synopsys --std=08 %source%/math_library/multiplier/multiplier_pkg.vhd
-ghdl -a --ieee=synopsys --std=08 %source%/math_library/sincos/sincos_pkg.vhd
+ghdl -a --ieee=synopsys --std=08 sincos/lut_generator_functions/sine_lut_generator_pkg.vhd
+ghdl -a --ieee=synopsys --std=08 sincos/lut_sine_pkg.vhd
 
-ghdl -a --ieee=synopsys --std=08 %source%/math_library/sincos/lut_generator_functions/sine_lut_generator_pkg.vhd
-ghdl -a --ieee=synopsys --std=08 %source%/math_library/sincos/lut_sine_pkg.vhd
+ghdl -a --ieee=synopsys --std=08 division/division_internal_pkg.vhd
+ghdl -a --ieee=synopsys --std=08 division/division_pkg.vhd
+ghdl -a --ieee=synopsys --std=08 first_order_filter/first_order_filter_pkg.vhd
+ghdl -a --ieee=synopsys --std=08 pi_controller/pi_controller_pkg.vhd
 
-ghdl -a --ieee=synopsys --std=08 %source%/math_library/division/division_internal_pkg.vhd
-ghdl -a --ieee=synopsys --std=08 %source%/math_library/division/division_pkg.vhd
-ghdl -a --ieee=synopsys --std=08 %source%/math_library/first_order_filter/first_order_filter_pkg.vhd
-ghdl -a --ieee=synopsys --std=08 %source%/math_library/pi_controller/pi_controller_pkg.vhd
-
-ghdl -a --ieee=synopsys --std=08 %source%/math_library/coordinate_transforms/abc_to_ab_transform/abc_to_ab_transform_pkg.vhd
-ghdl -a --ieee=synopsys --std=08 %source%/math_library/coordinate_transforms/abc_to_ab_transform/ab_to_abc_transform_pkg.vhd
-ghdl -a --ieee=synopsys --std=08 %source%/math_library/coordinate_transforms/ab_to_dq_transform/dq_to_ab_transform_pkg.vhd
-ghdl -a --ieee=synopsys --std=08 %source%/math_library/coordinate_transforms/ab_to_dq_transform/ab_to_dq_transform_pkg.vhd
+ghdl -a --ieee=synopsys --std=08 coordinate_transforms/abc_to_ab_transform/abc_to_ab_transform_pkg.vhd
+ghdl -a --ieee=synopsys --std=08 coordinate_transforms/abc_to_ab_transform/ab_to_abc_transform_pkg.vhd
+ghdl -a --ieee=synopsys --std=08 coordinate_transforms/ab_to_dq_transform/dq_to_ab_transform_pkg.vhd
+ghdl -a --ieee=synopsys --std=08 coordinate_transforms/ab_to_dq_transform/ab_to_dq_transform_pkg.vhd
 
