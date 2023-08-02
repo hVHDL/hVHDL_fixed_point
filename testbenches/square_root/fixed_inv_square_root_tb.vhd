9b30e7da0008dbd77a0f6ab386a260eb82d29a45
Jari Honkanen <jaarhonka@yahoo.com>
2023-08-03 00:49:39 +0300

    rename instances of sqrt to isqrt in testbench

--- testbenches/square_root/fixed_inv_square_root_tb.vhd
+++ testbenches/square_root/fixed_inv_square_root_tb.vhd
@@ -5,32 +5,36 @@ library ieee;
     use work.real_to_fixed_pkg.all;
     use work.multiplier_pkg.all;
 
-package fixed_sqrt_pkg is
-
-    type sqrt_record is record
-        x_squared      :  signed(int_word_length-1 downto 0);
-        x              :  signed(int_word_length-1 downto 0);
-        result         :  signed(int_word_length-1 downto 0);
+package fixed_isqrt_pkg is
+------------------------------------------------------------------------
+    type isqrt_record is record
+        x_squared        : signed(int_word_length-1 downto 0);
+        x                : signed(int_word_length-1 downto 0);
+        result           : signed(int_word_length-1 downto 0);
         sign_input_value : signed(int_word_length-1 downto 0);
-        state_counter  :  natural;                           
-        state_counter2 :  natural;                           
+        state_counter    : natural range 0 to 7;
+        state_counter2   : natural range 0 to 7;
+        isqrt_is_ready   : boolean;
     end record;
 
-    function to_fixed (
-        number : real;
-        radix : natural)
-    return signed;
-
-    function init_sqrt return sqrt_record;
-
-    procedure create_sqrt (
-        signal self : inout sqrt_record;
+    function init_isqrt return isqrt_record;
+------------------------------------------------------------------------
+    procedure create_isqrt (
+        signal self : inout isqrt_record;
         signal multiplier : inout multiplier_record);
+------------------------------------------------------------------------
+    procedure request_isqrt (
+        signal self : inout isqrt_record;
+        input_number : signed);
+------------------------------------------------------------------------
+    function isqrt_is_ready ( self : isqrt_record)
+        return boolean;
 
-end package fixed_sqrt_pkg;
+end package fixed_isqrt_pkg;
 
-package body fixed_sqrt_pkg is
+package body fixed_isqrt_pkg is
 
+------------------------------------------------------------------------
     function to_fixed
     (
         number : real;
@@ -42,9 +46,10 @@ package body fixed_sqrt_pkg is
         return to_fixed(number, int_word_length, radix);
     end to_fixed;
 
-    function init_sqrt return sqrt_record
+------------------------------------------------------------------------
+    function init_isqrt return isqrt_record
     is
-        variable returned_value : sqrt_record;
+        variable returned_value : isqrt_record;
     begin
         returned_value := (
          to_fixed(0.0   , 24)             ,
@@ -52,13 +57,14 @@ package body fixed_sqrt_pkg is
          to_fixed(0.0   , int_word_length , int_word_length-2) ,
          to_fixed(0.0   , int_word_length , int_word_length-2) ,
          0              ,
-         0);
+         0              ,
+         false);
          return returned_value;
-    end init_sqrt;
+    end init_isqrt;
 ------------------------------------------------------------------------
-    procedure create_sqrt
+    procedure create_isqrt
     (
-        signal self : inout sqrt_record;
+        signal self : inout isqrt_record;
         signal multiplier : inout multiplier_record
     ) is
         variable mult_result : signed(int_word_length-1 downto 0);
@@ -88,10 +94,31 @@ package body fixed_sqrt_pkg is
                 end if;
             WHEN others => --do nothign
         end CASE;
-        
-    end create_sqrt;
+    end create_isqrt;
 
-end package body fixed_sqrt_pkg;
+    procedure request_isqrt
+    (
+        signal self : inout isqrt_record;
+        input_number : signed
+    ) is
+    begin
+        self.sign_input_value <= input_number;
+        self.state_counter <= 0;
+        self.state_counter2 <= 0;
+    end request_isqrt;
+
+    function isqrt_is_ready
+    (
+        self : isqrt_record
+    )
+    return boolean
+    is
+    begin
+        return self.isqrt_is_ready;
+    end isqrt_is_ready;
+
+------------------------------------------------------------------------
+end package body fixed_isqrt_pkg;
 ------------------------------------------------------------------------
 LIBRARY ieee  ; 
     USE ieee.NUMERIC_STD.all  ; 
@@ -104,7 +131,7 @@ context vunit_lib.vunit_context;
     use work.square_root_pkg.all;
     use work.real_to_fixed_pkg.all;
     use work.multiplier_pkg.all;
-    use work.fixed_sqrt_pkg.all;
+    use work.fixed_isqrt_pkg.all;
 
 entity fixed_inv_square_root_tb is
   generic (runner_cfg : string);
@@ -126,7 +153,7 @@ architecture vunit_simulation of fixed_inv_square_root_tb is
     signal input_value : real := 1.0;
     signal output_value : real := 0.0;
 
-    signal inv_sqrt_is_ready : boolean := false;
+    signal inv_isqrt_is_ready : boolean := false;
     signal request_isqrt : boolean := false;
 
 
@@ -139,7 +166,7 @@ architecture vunit_simulation of fixed_inv_square_root_tb is
     signal multiplier : multiplier_record := init_multiplier;
 
 
-     signal self : sqrt_record := init_sqrt;
+     signal self : isqrt_record := init_isqrt;
 
 begin
 
@@ -158,7 +185,7 @@ begin
     simulator_clock <= not simulator_clock after clock_per/2.0;
 ------------------------------------------------------------------------
     stimulus : process(simulator_clock)
-        variable inv_sqrt_error : real;
+        variable inv_isqrt_error : real;
         variable mult_result : signed(self.x'range);
     begin
         if rising_edge(simulator_clock) then
@@ -166,27 +193,27 @@ begin
             create_multiplier(multiplier);
 
             request_isqrt <= false;
-            if inv_sqrt_is_ready then
+            if inv_isqrt_is_ready then
                 if input_value < 2.0 then
                     input_value <= input_value + 0.02;
                     request_isqrt <= true;
                 end if;
             end if;
 
-            inv_sqrt_is_ready <= false;
+            inv_isqrt_is_ready <= false;
             if request_isqrt then
-                inv_sqrt_is_ready <= true;
+                inv_isqrt_is_ready <= true;
                 sign_input_value <= to_fixed(input_value,sign_input_value'length, sign_input_value'high-2);
                 square_root_was_requested <= true;
             end if;
 
             CASE simulation_counter is
                 WHEN 10 =>
-                    inv_sqrt_is_ready <= true;
+                    inv_isqrt_is_ready <= true;
                 WHEN others => --do nothing
             end CASE;
 
-            create_sqrt(self, multiplier);
+            create_isqrt(self, multiplier);
 
         end if; -- rising_edge
     end process stimulus;	
