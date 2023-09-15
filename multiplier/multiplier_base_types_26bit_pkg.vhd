56a7cef7d6909f6e6658953c920cf43106f17064
Jari Honkanen <jaarhonka@yahoo.com>
2023-09-15 07:08:38 +0300

    add work. to pipeline registers

--- multiplier/multiplier_base_types_26bit_pkg.vhd
+++ multiplier/multiplier_base_types_26bit_pkg.vhd
@@ -8,8 +8,8 @@ library ieee;
 package multiplier_base_types_pkg is
 
     constant number_of_input_bits : integer := 26;
-    alias number_of_input_registers  is multiplier_pipeline_registers_pkg.input_registers;
-    alias number_of_output_registers is multiplier_pipeline_registers_pkg.output_registers;
+    alias number_of_input_registers  is work.multiplier_pipeline_registers_pkg.input_registers;
+    alias number_of_output_registers is work.multiplier_pipeline_registers_pkg.output_registers;
     subtype int is integer range -2**(number_of_input_bits-1) to 2**(number_of_input_bits-1)-1;
 
 end package multiplier_base_types_pkg;
--- multiplier/multiplier_base_types_for_sqrt_pkg.vhd
+++ multiplier/multiplier_base_types_for_sqrt_pkg.vhd
@@ -9,8 +9,8 @@ library ieee;
 package multiplier_base_types_pkg is
 
     constant number_of_input_bits : integer := 55; -- odd lengths works if above 35 bits
-    alias number_of_input_registers  is multiplier_pipeline_registers_pkg.input_registers;
-    alias number_of_output_registers is multiplier_pipeline_registers_pkg.output_registers;
+    alias number_of_input_registers  is work.multiplier_pipeline_registers_pkg.input_registers;
+    alias number_of_output_registers is work.multiplier_pipeline_registers_pkg.output_registers;
     subtype int is integer;
 
 end package multiplier_base_types_pkg;
