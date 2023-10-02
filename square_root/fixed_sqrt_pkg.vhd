c6a11b0262f5b2b1cac3331d47d9cc36e2fe9585
Jari Honkanen <jaarhonka@yahoo.com>
2023-10-02 07:02:49 +0300

    add number of iterations to sqrt record

--- square_root/fixed_sqrt_pkg.vhd
+++ square_root/fixed_sqrt_pkg.vhd
@@ -12,9 +12,12 @@ package fixed_sqrt_pkg is
     constant used_word_length : natural := int_word_length;
     subtype fixed is signed(used_word_length-1 downto 0);
 
+    constant default_number_of_iterations : integer := 5;
+
     type fixed_sqrt_record is record
         isqrt                 : isqrt_record;
         shift_width           : natural;
+        number_of_iterations  : natural range 1 to 7;
         input                 : fixed;
         scaled_input          : fixed;
         pipeline              : std_logic_vector(2 downto 0);
@@ -22,7 +25,7 @@ package fixed_sqrt_pkg is
         sqrt_is_ready         : boolean;
     end record;
 
-    constant init_sqrt : fixed_sqrt_record := (init_isqrt, 0 , (others => '0'), (others => '0'), (others => '0'), false, false);
+    constant init_sqrt : fixed_sqrt_record := (init_isqrt, 0, default_number_of_iterations, (others => '0'), (others => '0'), (others => '0'), false, false);
 ------------------------------------------------------------------------
     procedure create_sqrt (
         signal self       : inout fixed_sqrt_record;
@@ -51,7 +54,6 @@ package body fixed_sqrt_pkg is
         signal self       : inout fixed_sqrt_record;
         signal multiplier : inout multiplier_record
     ) is
-        constant number_of_iterations : integer := 5;
     begin
         create_isqrt(self.isqrt, multiplier);
 
@@ -60,7 +62,7 @@ package body fixed_sqrt_pkg is
         self.shift_width  <= get_number_of_leading_pairs_of_zeros(self.input);
 
         if self.pipeline(self.pipeline'left) = '1' then
-            request_isqrt(self.isqrt, self.scaled_input, get_initial_guess(self.scaled_input),number_of_iterations);
+            request_isqrt(self.isqrt, self.scaled_input, get_initial_guess(self.scaled_input), self.number_of_iterations);
         end if;
 
         if isqrt_is_ready(self.isqrt) then
