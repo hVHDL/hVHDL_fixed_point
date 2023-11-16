1ad7cb0822a3cc05cc5639d42a88a88fe7258b56
Jari Honkanen <jaarhonka@yahoo.com>
2023-11-16 07:56:31 +0200

    remove aliases from sincos

--- sincos/sincos_pkg.vhd
+++ sincos/sincos_pkg.vhd
@@ -22,20 +22,20 @@ package sincos_pkg is
     constant init_sincos : sincos_record := (15, (others => '0'), 0, 0, 0, 0, 0, false);
 ------------------------------------------------------------------------
     procedure request_sincos (
-        signal sincos_object : inout sincos_record;
+        signal self : inout sincos_record;
         angle_rad16 : in int);
 
     procedure request_sincos (
-        signal sincos_object : inout sincos_record;
+        signal self : inout sincos_record;
         angle_rad16 : in unsigned);
 ------------------------------------------------------------------------
-    function get_sine ( sincos_object : sincos_record)
+    function get_sine ( self : sincos_record)
         return int;
 ------------------------------------------------------------------------
-    function get_cosine ( sincos_object : sincos_record)
+    function get_cosine ( self : sincos_record)
         return int;
 ------------------------------------------------------------------------
-    function sincos_is_ready ( sincos_object : sincos_record)
+    function sincos_is_ready ( self : sincos_record)
         return boolean;
 ------------------------------------------------------------------------
     function angle_reduction ( angle_in_rad16 : int)
@@ -43,7 +43,7 @@ package sincos_pkg is
 ------------------------------------------------------------------------
     procedure create_sincos (
         signal hw_multiplier : inout multiplier_record;
-        signal sincos_object : inout sincos_record);
+        signal self : inout sincos_record);
 ------------------------------------------------------------------------ 
 end package sincos_pkg;
 
@@ -64,80 +64,72 @@ package body sincos_pkg is
 ------------------------------------------------------------------------ 
     function sincos_is_busy
     (
-        sincos_object : sincos_record
+        self : sincos_record
     )
     return boolean
     is
     begin
-        return sincos_object.sincos_process_counter <= 8;
+        return self.sincos_process_counter <= 8;
     end sincos_is_busy;
 ------------------------------------------------------------------------ 
     procedure request_sincos
     (
-        signal sincos_object : inout sincos_record;
+        signal self : inout sincos_record;
         angle_rad16 : in int
     ) is
     begin
-            request_sincos(sincos_object, to_unsigned(angle_rad16, 16));
+            request_sincos(self, to_unsigned(angle_rad16, 16));
         
     end request_sincos;
 ------------------------------------------------------------------------ 
     procedure request_sincos
     (
-        signal sincos_object : inout sincos_record;
+        signal self : inout sincos_record;
         angle_rad16 : in unsigned
     ) is
     begin
-        if sincos_object.sincos_process_counter >= 8 then
-            sincos_object.angle_rad16 <= angle_rad16;
-            sincos_object.sincos_process_counter <= 0;
+        if self.sincos_process_counter >= 8 then
+            self.angle_rad16 <= angle_rad16;
+            self.sincos_process_counter <= 0;
         end if;
         
     end request_sincos;
 ------------------------------------------------------------------------ 
     function get_sine
     (
-        sincos_object : sincos_record
+        self : sincos_record
     )
     return int
     is
     begin
-        return sincos_object.sin;
+        return self.sin;
     end get_sine;
 ------------------------------------------------------------------------ 
     function get_cosine
     (
-        sincos_object : sincos_record
+        self : sincos_record
     )
     return int
     is
     begin
-        return sincos_object.cos;
+        return self.cos;
     end get_cosine;
 ------------------------------------------------------------------------ 
     function sincos_is_ready
     (
-        sincos_object : sincos_record
+        self : sincos_record
     )
     return boolean
     is
     begin
-        return sincos_object.sincos_has_finished;
+        return self.sincos_has_finished;
     end sincos_is_ready;
 ------------------------------------------------------------------------ 
     procedure create_sincos
     (
         signal hw_multiplier : inout multiplier_record;
-        signal sincos_object : inout sincos_record
+        signal self : inout sincos_record
     ) is
-        alias sincos_process_counter is sincos_object.sincos_process_counter ;
-        alias angle_rad16            is sincos_object.angle_rad16            ;
-        alias angle_squared          is sincos_object.angle_squared          ;
-        alias sin16                  is sincos_object.sin16                  ;
-        alias cos16                  is sincos_object.cos16                  ;
-        alias sin                    is sincos_object.sin                    ;
-        alias cos                    is sincos_object.cos                    ;
-        alias sincos_has_finished    is sincos_object.sincos_has_finished    ;
 
         type int_array is array (integer range <>) of int;
         constant sinegains : int_array(0 to 2) := (12868 , 21159 , 10180);
@@ -149,65 +141,65 @@ package body sincos_pkg is
         constant seven_fourths : integer := 57344 ;
 ------------------------------------------------------------------------
     begin
-            sincos_has_finished <= false;
+            self.sincos_has_finished <= false;
 
-            CASE sincos_process_counter is
+            CASE self.sincos_process_counter is
                 WHEN 0 => 
-                    multiply(hw_multiplier, angle_reduction(to_integer(angle_rad16)), angle_reduction(to_integer(angle_rad16)));
-                    sincos_process_counter <= sincos_process_counter + 1;
+                    multiply(hw_multiplier, angle_reduction(to_integer(self.angle_rad16)), angle_reduction(to_integer(self.angle_rad16)));
+                    self.sincos_process_counter <= self.sincos_process_counter + 1;
                 WHEN 1 =>
                     if multiplier_is_ready(hw_multiplier) then
-                        angle_squared <= get_multiplier_result(hw_multiplier, 15);
+                        self.angle_squared <= get_multiplier_result(hw_multiplier, 15);
                         multiply(hw_multiplier,                sinegains(2), get_multiplier_result(hw_multiplier, 15));
                     end if;
-                    increment_counter_when_ready(hw_multiplier,sincos_process_counter);
+                    increment_counter_when_ready(hw_multiplier,self.sincos_process_counter);
                 WHEN 3 =>
                     if multiplier_is_ready(hw_multiplier) then 
-                        multiply(hw_multiplier, angle_squared, sinegains(1) - get_multiplier_result(hw_multiplier, 15)); 
+                        multiply(hw_multiplier, self.angle_squared, sinegains(1) - get_multiplier_result(hw_multiplier, 15)); 
                     end if;
-                    increment_counter_when_ready(hw_multiplier,sincos_process_counter);
+                    increment_counter_when_ready(hw_multiplier,self.sincos_process_counter);
                 WHEN 5 =>
                     if multiplier_is_ready(hw_multiplier) then
-                        multiply(hw_multiplier, angle_reduction((to_integer(angle_rad16))), sinegains(0) - get_multiplier_result(hw_multiplier, 15)); 
+                        multiply(hw_multiplier, angle_reduction((to_integer(self.angle_rad16))), sinegains(0) - get_multiplier_result(hw_multiplier, 15)); 
                     end if;
-                    increment_counter_when_ready(hw_multiplier,sincos_process_counter);
+                    increment_counter_when_ready(hw_multiplier,self.sincos_process_counter);
                 WHEN 7 =>
                     if multiplier_is_ready(hw_multiplier) then
-                        sin16 <= get_multiplier_result(hw_multiplier,12);
+                        self.sin16 <= get_multiplier_result(hw_multiplier,12);
                     end if;
-                    increment_counter_when_ready(hw_multiplier,sincos_process_counter); 
+                    increment_counter_when_ready(hw_multiplier,self.sincos_process_counter); 
                 WHEN others => -- do nothing
             end CASE;
 
-            CASE sincos_process_counter is
+            CASE self.sincos_process_counter is
                 WHEN 2 =>
-                    multiply(hw_multiplier, angle_squared, cosgains(2));
-                    sincos_process_counter <= sincos_process_counter + 1;
+                    multiply(hw_multiplier, self.angle_squared, cosgains(2));
+                    self.sincos_process_counter <= self.sincos_process_counter + 1;
                 WHEN 4 =>
-                    multiply(hw_multiplier, angle_squared, cosgains(1) - get_multiplier_result(hw_multiplier, 15));
-                    sincos_process_counter <= sincos_process_counter + 1;
+                    multiply(hw_multiplier, self.angle_squared, cosgains(1) - get_multiplier_result(hw_multiplier, 15));
+                    self.sincos_process_counter <= self.sincos_process_counter + 1;
                 WHEN 6 => 
-                    cos16 <= cosgains(0) - get_multiplier_result(hw_multiplier, 14);
-                    sincos_process_counter <= sincos_process_counter + 1;
+                    self.cos16 <= cosgains(0) - get_multiplier_result(hw_multiplier, 14);
+                    self.sincos_process_counter <= self.sincos_process_counter + 1;
                 WHEN 8 =>
-                    sincos_process_counter <= sincos_process_counter + 1;
-                    sincos_has_finished <= true;
+                    self.sincos_process_counter <= self.sincos_process_counter + 1;
+                    self.sincos_has_finished <= true;
 
-                    if (to_integer(angle_rad16)) < one_quarter then
-                        sin <= sin16;
-                        cos <= cos16;
-                    elsif (to_integer(angle_rad16)) < three_fourths then
-                        sin <= cos16;
-                        cos <= -sin16;
-                    elsif (to_integer(angle_rad16)) < five_fourths then
-                        sin <= -sin16;
-                        cos <= -cos16;
-                    elsif (to_integer(angle_rad16)) < seven_fourths then
-                        sin <= -cos16;
-                        cos <= sin16;
+                    if (to_integer(self.angle_rad16)) < one_quarter then
+                        self.sin <= self.sin16;
+                        self.cos <= self.cos16;
+                    elsif (to_integer(self.angle_rad16)) < three_fourths then
+                        self.sin <= self.cos16;
+                        self.cos <= -self.sin16;
+                    elsif (to_integer(self.angle_rad16)) < five_fourths then
+                        self.sin <= -self.sin16;
+                        self.cos <= -self.cos16;
+                    elsif (to_integer(self.angle_rad16)) < seven_fourths then
+                        self.sin <= -self.cos16;
+                        self.cos <= self.sin16;
                     else
-                        sin <= sin16;
-                        cos <= cos16;
+                        self.sin <= self.sin16;
+                        self.cos <= self.cos16;
                     end if;
 
                 when others => -- hange here and wait for triggering
