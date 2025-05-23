6713b69f1def47841539181e0125760ef803b4a5
Jari Honkanen <jaarhonka@yahoo.com>
2025-05-23 19:52:35 +0300

    start moving reciprocal to own record

--- testbenches/division_simulation/sequential_zero_shift_tb.vhd
+++ testbenches/division_simulation/sequential_zero_shift_tb.vhd
@@ -34,35 +34,48 @@ architecture vunit_simulation of seq_zero_shift_tb is
     -----------------------------------
     -- simulation specific signals ----
     signal multiplier : multiplier_record := init_multiplier;
-    signal self       : division_record   := init_division;
-
 
     constant wordlength : natural := 36;
     constant radix : natural := wordlength-3;
 
     signal x1 : signed(wordlength-1 downto 0) := to_fixed(0.5, wordlength, radix);
     signal x2 : signed(wordlength-1 downto 0) := to_fixed(0.5, wordlength, radix);
-
     signal xi : signed(wordlength-1 downto 0) := to_fixed(1.0/1.7, wordlength, radix);
 
-    signal a  : signed(wordlength-1 downto 0) := to_fixed(8.0 * 1.7*1.1, wordlength, radix-5);
+    signal a  : signed(wordlength-1 downto 0) := to_fixed(88.95, wordlength, radix-5);
     signal b  : signed(wordlength-1 downto 0) := to_fixed(1.7, wordlength, radix);
 
     signal b_div_a : signed(wordlength-1 downto 0) := to_fixed(0.0, wordlength, radix);
 
     signal result : real := 0.0;
     signal inv_a : real := 0.0;
+    signal ref_a : real := 0.0;
 
     signal input_shift_register : unsigned(wordlength-2 downto 0) := (others => '1');
     signal input_zero_count     : natural   := 5;
 
+    signal inv_a_out : a'subtype := (others => '0');
+
     signal output_shift_register : a'subtype := (0 => '1' , others => '0');
     signal output_shift_count     : natural   := 0;
 
-    signal seq_count   : natural := 3;
     constant max_shift : natural := 8;
 
-    signal iteration_count : natural := 4;
+    type reciprocal_record is record
+        seq_count       : natural range 0 to 7;
+        iteration_count : natural range 0 to 7;
+        x1              : signed(wordlength-1 downto 0);
+        xi              : signed(wordlength-1 downto 0);
+    end record;
+
+    constant init_reciproc : reciprocal_record := (
+        seq_count         => 3
+        , iteration_count => 7
+        , x1              => to_fixed(0.5, wordlength, radix)
+        , xi              => to_fixed(1.0/1.7, wordlength, radix)
+    );
+
+    signal self : reciprocal_record := init_reciproc;
 
 begin
 
@@ -115,45 +128,63 @@ begin
         if rising_edge(simulator_clock) then
             simulation_counter <= simulation_counter + 1;
 
+            -- unneeded right now
+            if output_shift_count > 0
+            then
+                -- output_shift_count <= 
+                if output_shift_count > 3 then
+                    output_shift_count <= output_shift_count-3;
+                    output_shift_register <= shift_left(output_shift_register,3);
+                else
+                    output_shift_count <= 0;
+                    output_shift_register <= shift_left(output_shift_register,output_shift_count);
+                end if;
+            end if;
+            ----
+
             input_zero_count     <= input_zero_count + number_of_leading_zeroes(input_shift_register, max_shift => max_shift);
             input_shift_register <= shift_left(
                                     input_shift_register
                                     ,(number_of_leading_zeroes(input_shift_register, max_shift => max_shift)));
 
-            if output_shift_count > 0
-            then
-                -- output_shift_count <= 
-            end if;
-
-            CASE seq_count is
+            CASE self.seq_count is
                 WHEN 0 => 
                     if number_of_leading_zeroes(input_shift_register, max_shift => max_shift) = 0
                     then
-                        seq_count <= seq_count + 1;
+                        self.seq_count <= self.seq_count + 1;
                     end if;
                 WHEN 1 => 
-                    x1 <= inv_mantissa(mpy(xi,signed("00" & input_shift_register(input_shift_register'left downto 1) )));
+                    self.x1 <= inv_mantissa(mpy(self.xi,signed("00" & input_shift_register(input_shift_register'left downto 1) )));
 
-                    seq_count <= seq_count + 1;
+                    self.seq_count <= self.seq_count + 1;
                 WHEN 2 => 
-                    xi <= mpy(xi, x1);
+                    self.xi <= mpy(self.xi, self.x1);
 
-                    if iteration_count > 0
+                    if self.iteration_count > 0
                     then
-                        iteration_count <= iteration_count -1;
-                        seq_count <= 1;
+                        self.iteration_count <= self.iteration_count -1;
+                        self.seq_count <= 1;
                     else
-                        seq_count <= seq_count + 1;
+                        self.seq_count <= self.seq_count + 1;
                         output_shift_count <= input_zero_count;
+                        output_shift_register <= self.x1;
+                        -- get from dsp output register
+                        inv_a_out <= signed(resize(shift_right(self.xi,6), inv_a_out'length));
                     end if;
 
                 WHEN 3 => 
-                    b_div_a <= mpy(b,xi);
-                    seq_count <= seq_count + 1;
+                    b_div_a <= mpy(b,self.xi);
+                    self.seq_count <= self.seq_count + 1;
                 WHEN others => -- do nothing
+
             end CASE;
 
-            inv_a <= to_real(shift_right(xi,output_shift_count) , radix);
+            inv_a <= to_real(inv_a_out, radix);
+
+            if inv_a /= 0.0
+            then
+                ref_a <= 1.0/inv_a;
+            end if;
 
             if a > 0 then
                 result <= to_real(b_div_a, radix);
@@ -165,8 +196,8 @@ begin
                 WHEN 0 =>
                     input_shift_register <= unsigned(a(input_shift_register'range));
                     input_zero_count <= 0;
-                    iteration_count <= 5;
-                    seq_count <= 0;
+                    self.iteration_count <= 5;
+                    self.seq_count <= 0;
                 WHEN others => -- do nothing
             end CASE;
 
